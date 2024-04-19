4765ed5ae65756e74e489cfb0d0e8a5334434f9a
Gokulnath Anand <gokulnathanand23@gmail.com>
2024-04-18 23:01:37 -0400

    changes to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/about", "/publications", "blog"]
+  publicRoutes: ["/", "/about", "/publications", "/blog"]
 });
 
 export const config = {
